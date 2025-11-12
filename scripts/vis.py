7cb9ed6b684d940c0e5e76f97842fc211d96cd90
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-11-13 01:13:30 +0600

    Add initial implementation of image processing and generation in vis.py

---
+++ scripts/vis.py
@@ -0,0 +1,20 @@
+from transformers import Qwen3VLForConditionalGeneration, AutoProcessor
+from qwen_vl_utils import process_vision_info
+
+model_path = "Qwen/Qwen3-VL-8B-Instruct"
+# You can directly insert a local file path, a URL, or a base64-encoded image into the position where you want in the text.
+messages = [
+    [{"role": "user", "content": [{"type": "image", "image": "../fordcasepage3.png"}, {"type": "text", "text": "Describe this image."}]}],
+]
+
+processor = AutoProcessor.from_pretrained(model_path)
+model = Qwen3VLForConditionalGeneration.from_pretrained(model_path, dtype="auto", device_map="auto")
+
+text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+images = process_vision_info(messages, image_patch_size=16)
+
+inputs = processor(text=text, images=images, return_tensors="pt", do_resize=False)
+inputs = inputs.to(model.device)
+
+generated_ids = model.generate(**inputs)
+print(generated_ids)
