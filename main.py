2af238c874a957bc33304e3a80f83c759720e7cf
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-08-27 01:28:51 +0600

    Update LLM API base URL to point to the correct server

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ from llama_index.core.llms import ChatMessage
 
 
 llm = OpenAILike(
-    api_base="http://vast02:7600/", # Server uses local LLM
+    api_base="http://174.119.98.154:42063/", # Server uses local LLM
     # api_base="https://llamat.emediaworkspace.com/", # Use this for testing locally with the remote LLM
     is_chat_model=True,
     is_function_calling_model=True
