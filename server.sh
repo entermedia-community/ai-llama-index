3e43cba1750f171e8fa2daeaed40be1d2b6fb352
Cristobal Mejia <cristobal.mejia@gmail.com>
2026-09-01 11:53:48 -0600

    server launcher

--- server.sh
+++ server.sh
@@ -5,9 +5,12 @@ if [[ -f ".venv/bin/activate" ]]; then
   source .venv/bin/activate
 fi
 
+LOGFILE=/root/logs/llamaindex
+mkdir -p "$LOGFILE"
+
 # export CUDA_VISIBLE_DEVICES=1
 python -m uvicorn main:app \
 	--host 0.0.0.0 \
 	--port 4600 \
 	--timeout-keep-alive 120 \
-	--workers 1 > /dev/null 2>&1 &
+	--workers 1 > /dev/null 2>&1 | multilog t s5000000 n3 "$LOGFILE" &
